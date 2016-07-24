1f1f23f508ff1b2507a53b747d0b1ce36b3a7158
Jeremy Shepherd <jeremylshepherd@gmail.com>
2016-07-24 18:03:43 +0000

    Added Routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Term = require('../models/SearchTerms');
+var Seed = require('../mocks/seed');
+var Mock = require("../mocks/mockData");
+var Search = require('bing.search');
+var util = require('util');
+
+function genJSON(doc) {
+    return doc;
+}
+
 
 
 router.get('/', function(req, res) {
@@ -23,6 +34,43 @@ router.post('/api/newrecord', function(req, res) {
     });
 });
 
+// router.get('/api/:term', function(req, res) {
+//     var term = req.params.term;
+//     var obj = {};
+//     obj.term = term;
+//     var record = new Term(obj);
+//     record.save((err, record) => {if(err) {console.log(err);} console.log('Record saved.')});
+    
+//     var search = new Search(process.env.BING_SECRET);
+ 
+//     search.images(
+//         'Tutta Bella Neapolitan Pizzeria',
+//         {top: 5},
+//         function(err, results) {
+//             if(err){console.log(err);}
+//             console.log(util.inspect(results, 
+//             {colors: true, depth: null}));
+//             res.json(results);
+//      });
+// });
+
+router.get('/api/mocksearch', function(req, res) {
+   var nu = genJSON(Mock);
+   res.json(nu);
+});
+
+router.get('/noapi/:string/:offset', function(req, res) {
+   var string = req.params.string;
+   var offset = req.params.offset || 0;
+   if(offset < 1){
+       res.json(string);
+   }else{
+       var obj = {};
+       obj.string = string;
+       obj.offset = offset;
+       res.json(obj);
+   }
+});
 
 router.get('/api/allterms', function(req, res) {
     Term.find({}, function(err, results) {
